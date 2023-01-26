43d463cfbb9fd9924a742b17db8a0643f9f99d23
mrshahriar <toshahriarshakil@gmail.com>
2023-01-26 21:02:05 +0600

    update env key

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "/";
+const url = process.env.NODE_ENV === "production" ? process.env.API_URL : "/";
 export const api = axios.create({
   baseURL: url,
   withCredentials: true,
