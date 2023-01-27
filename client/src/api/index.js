5882b2793127a61271a6790e1062f56f931917bd
mrshahriar <toshahriarshakil@gmail.com>
2023-01-27 23:29:37 +0600

    porxy change

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 //   process.env.NODE_ENV === "production" ? process.env.REACT_APP_API_URL : "/";
 
 export const api = axios.create({
-  baseURL: "/",
+  baseURL: "https://memories-project-mern.shahriar-shakil.repl.co/",
   withCredentials: true,
   headers: {
     "Access-Control-Allow-Origin": "*",
