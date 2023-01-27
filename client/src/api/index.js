67c39d0848ae55dfa34f4dff4709ae2bb14ff615
mrshahriar <toshahriarshakil@gmail.com>
2023-01-27 23:26:09 +0600

    fix lint error

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const url =
-  process.env.NODE_ENV === "production" ? process.env.REACT_APP_API_URL : "/";
+// const url =
+//   process.env.NODE_ENV === "production" ? process.env.REACT_APP_API_URL : "/";
 
 export const api = axios.create({
   baseURL: "/",
